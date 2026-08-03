993b977f37f62d306d53a6430887f4bbc2657819
Abdulqadirmachiwala <abdulpc.21@gmail.com>
2026-08-03 21:46:11 +0530

    Initial project setup

---
+++ Server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Database
+connectDB();
+
+// Test Route
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome to IndieGamer Hub API 🚀"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
